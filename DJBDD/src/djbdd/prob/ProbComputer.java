164fb848d10167bbaefeddf78794d9cbc0a0cecc
diegoj <diegojromerolopez@gmail.com>
2013-08-23 18:44:26 +0200

    Fixing comments and basic refactoring.

--- DJBDD/src/djbdd/prob/ProbComputer.java
+++ DJBDD/src/djbdd/prob/ProbComputer.java
@@ -8,23 +8,60 @@ import djbdd.*;
 import java.util.*;
 
 /**
- *
+ * Computes the Probabilities of the Variables of a Boolean Formula using Binary Decision Diagrams.
+ * Based on the scientific paper of David Fernandez-Amoros & Ruben Heradio.
  * @author diegoj
  */
 public class ProbComputer {
     
     /** BDD whose variable probabilites will be computed */
     private final BDD bdd;
+    
+    /**
+     * Vertices of the BDD
+     * Each vertex has some position in this list that is different than its index.
+     */
     private final ArrayList<Vertex> vertices;
+    
+    /**
+     * Number of vertices of the BDD.
+     */
     private final int size;
+    
+    /**
+     * This algorithm uses a different unique id for each vertex.
+     * Each vertex has as id the position in the vertices list.
+     * The key is the index of each vertex, the value is its index in the list of vertices.
+     */
     private final HashMap<Integer,Integer> ids;
+
+    /**
+     * Hashmaps that assigns to each vertex its variable unless it is a leaf vertex.
+     * The leaves have both the number of variables as variable index.
+     * The key is the index of each vertex, the value is the varible index.
+     */
     private final HashMap<Integer,Integer> probVariableIndices;
+    
+    /**
+     * Boolean mark that keeps memory of the visited vertices.
+     * The key is the index of each vertex.
+     */
     private HashMap<Integer,Boolean> visited;
+    
+    /**
+     * Number of variables.
+     */
     private final int numVariables;
     
-    /** Should we have to show aditional information */
+    /**
+     * Should we have to show aditional information
+     */
     private static final boolean VERBOSE = true;
     
+    /**
+     * Constructs the object that will compute the variable probability of a BDD.
+     * @param bdd BDD whose variable probability will be computed.
+     */
     public ProbComputer(BDD bdd){
         
         // Erase orphan nodes
@@ -65,19 +102,22 @@ public class ProbComputer {
             i++;
         }
         
+        // The leaves has has variable index the number of variables
         this.probVariableIndices.put(Vertex.FALSE_INDEX, this.numVariables);
         this.probVariableIndices.put(Vertex.TRUE_INDEX, this.numVariables);
         
+        // The root has the last position in the list of vertices
         this.vertices.add(root);
         this.ids.put(root.index, this.vertices.size()-1);
         this.probVariableIndices.put(root.index, root.variable());
         
-        // Have been some vertex visited?
+        // Mark all vertices as not-visited
         this.visited = new HashMap<Integer,Boolean>();
         for(Vertex v : this.vertices){
             this.visited.put(v.index,false);
         }
         
+        // Show information if needed
         if(VERBOSE){
             System.out.println("BDD Array");
             System.out.println("pos\tvar\tvar_name\tlow\thigh");
@@ -101,18 +141,42 @@ public class ProbComputer {
         }
     }
 
+    /* Functions that will make the code clearer */
+    
+    
+    /**
+     * Returns the position in the list of vertices of a vertex.
+     * @param v Vertex.
+     * @return Position of vertex v in the list of vertices.
+     */
     private int id(Vertex v){
         return this.ids.get(v.index);
     }
-    
+
+    /**
+     * Returns the variable of a vertex. Note the leaves has the number of variables as variable.
+     * @param v Vertex.
+     * @return Variable of a vertex.
+     */
     private int pVariableIndex(Vertex v){
         return this.probVariableIndices.get(v.index);
     }
     
+    /**
+     * Informs if a vertex has been visited.
+     * @param v Vertex.
+     * @return Is the vertex v visited?
+     */
     private boolean visited(Vertex v){
         return this.visited.get(v.index);
     }
-    
+
+    /**
+     * Sets the visited parameter of a vertex.
+     * @param v Vertex whose visited state will be changed.
+     * @param value Value that will be assigned to visited.
+     * @return New value for the visited attribute for a vertex.
+     */
     private boolean setVisited(Vertex v, boolean value){
         this.visited.put(v.index, value);
         return value;
@@ -158,6 +222,10 @@ public class ProbComputer {
 
     /**
      * Marginal probabilites.
+     * @param vIndex Index of a vertexc in the list of vertices.
+     * @param total_prob List of total probabilities. That is, the probability of having True or False.
+     * @param formula_sat_prob
+     * @param prob Marginal probabilities. IS MODIFIED. 
      */
     protected void MP_xi(int vIndex, double[] total_prob, double[] formula_sat_prob, double[] prob){
         double prob_low = 0.0;
@@ -205,7 +273,11 @@ public class ProbComputer {
         }
     }
 
-    public double[] P_xi(){
+    /**
+     * Gets the probability of each variable.
+     * @return List which each element is the probability of having True.
+     */
+    private double[] P_xi(){
         int numLiterals = this.numVariables;
         int numVertices = this.vertices.size();
 
@@ -226,13 +298,21 @@ public class ProbComputer {
         return prob;
     }
     
-    public void run(){
+    /**
+     * Execute the algorithm that computes the probabilities for the variables.
+     * @return The list of probabilities for each variable.
+     */
+    public double[] run(){
         double[] probs = this.P_xi();
         
-        for(double p : probs){
-            System.out.print(p+", ");
+        if(VERBOSE){
+            for(double p : probs){
+                System.out.print(p+", ");
+            }
+            System.out.println("");
         }
-        System.out.println("");
+        
+        return probs;
     }
     
 }
